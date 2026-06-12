64b9e8663742e690195d308ab0d71405683c310e
FlySolarLink <contact@flysolarlink.com>
2026-06-12 20:26:35 +0200

    Fix main block

--- app.py
+++ app.py
@@ -687,10 +687,8 @@ def build_bav_pdf(d):
     return buf
 
 
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
-    exit()
-    import os
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
