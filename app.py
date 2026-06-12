9bb856103e4ed77703f8b119566162f01529eb29
FlySolarLink <contact@flysolarlink.com>
2026-06-12 20:19:48 +0200

    Fix PORT Railway

--- app.py
+++ app.py
@@ -689,4 +689,8 @@ def build_bav_pdf(d):
 
 if __name__ == "__main__":
     import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+    exit()
+    import os
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
